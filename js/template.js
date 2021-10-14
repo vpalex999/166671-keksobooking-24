262d7ed155fd56beae95f1a70abcc5e08fa65845
vpalex999 <vpalex@mail.ru>
2021-10-14 20:25:39 +0500

    continue make template notice

--- js/template.js
+++ js/template.js
@@ -1,7 +1,6 @@
 import { createWizardNotice } from './data.js';
 
 const getNameOfPlace = (type) => {
-
   switch (type) {
     case 'flat':
       return 'Квартира';
@@ -18,16 +17,40 @@ const getNameOfPlace = (type) => {
   }
 };
 
-const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+const cardTemplate = document.querySelector('#card')
+  .content.querySelector('.popup');
 console.log(cardTemplate);
 
 
 const cardElement = cardTemplate.cloneNode(true);
-const notice = createWizardNotice();
-console.log(notice);
+console.log('ElNode: ', cardElement);
 
-cardElement.children[1].textContent = notice.offer.title;
-cardElement.children[2].textContent = notice.offer.address;
-cardElement.children[3].textContent = `${notice.offer.price} ₽/ночь`;
-cardElement.children[4].textContent = getNameOfPlace(notice.offer.type);
-console.log(cardElement);
+const notice = createWizardNotice();
+console.log('Notice: ', notice);
+
+cardElement.querySelector('.popup__title').textContent = notice.offer.title;
+cardElement.querySelector('.popup__text--address').textContent = notice.offer.address;
+cardElement.querySelector('.popup__text--price').firstChild.textContent = `${notice.offer.price} `;
+cardElement.querySelector('.popup__type').textContent = getNameOfPlace(notice.offer.type);
+cardElement.querySelector('.popup__text--capacity').textContent = `${notice.offer.rooms} комнаты для ${notice.offer.guests} гостей`;
+cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${notice.offer.checkin}, выезд до ${notice.offer.checkout}`;
+
+const featuresElement = cardElement.querySelectorAll('.popup__feature');
+featuresElement.forEach((featureElement) => {
+  const isNecessary = notice.offer.features.some(
+    (noticeFeature) => featureElement.classList.contains(`popup__feature--${noticeFeature}`)
+  );
+  if(!isNecessary){
+    featureElement.remove();
+  }
+});
+
+cardElement.querySelector('.popup__description').textContent = notice.offer.description;
+
+const photos = cardElement.querySelector('.popup__photos');
+
+
+console.log('FE2: ', cardElement);
+
+
+console.log('El1: ', cardElement.textContent);
