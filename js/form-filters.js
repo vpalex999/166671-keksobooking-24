b1ce89253f4d35c01c22a15eb9083259b8513003
vpalex999 <vpalex@mail.ru>
2021-11-09 21:15:53 +0500

    fix B24

--- js/form-filters.js
+++ js/form-filters.js
@@ -21,92 +21,81 @@ const getPopupContentElement = (marker, selector) => marker
   .getContent()
   .querySelector(selector);
 
-const filterHousingType = (markerList) => {
+const filterHousingType = (marker) => {
   const value = housingTypeSelectElement.value;
   if (value === 'any') {
-    return [...markerList];
+    return true;
   }
-  return markerList.filter((marker) => {
-    const element = getPopupContentElement(marker, '.popup__type');
-    return element.textContent === getNameOfPlace(value);
-  });
+  const element = getPopupContentElement(marker, '.popup__type');
+  return element.textContent === getNameOfPlace(value);
 };
 
-const filterHousingPrice = (markerList) => {
+const filterHousingPrice = (marker) => {
   const value = housingPriceSelectElement.value;
   if (value === 'any') {
-    return [...markerList];
+    return true;
   }
-  return markerList.filter((marker) => {
-    const element = getPopupContentElement(marker, '.popup__text--price');
-    const [minimal, maximal] = getNameOfPrice(value);
-    const price = Number(element.firstChild.textContent);
-    return price >= minimal && price <= maximal;
-  });
+  const element = getPopupContentElement(marker, '.popup__text--price');
+  const [minimal, maximal] = getNameOfPrice(value);
+  const price = Number(element.firstChild.textContent);
+  return price >= minimal && price <= maximal;
 };
 
-const filterHousingRooms = (markerList) => {
+const filterHousingRooms = (marker) => {
   const value = housingRoomsSelectElement.value;
   if (value === 'any') {
-    return [...markerList];
+    return true;
   }
-  return markerList.filter((marker) => {
-    const element = getPopupContentElement(marker, '.popup__text--capacity');
-    return element.firstChild.textContent.startsWith(value);
-  });
+  const element = getPopupContentElement(marker, '.popup__text--capacity');
+  return element.firstChild.textContent.startsWith(value);
 };
 
-const filterHousingGuests = (markerList) => {
+const filterHousingGuests = (marker) => {
   const value = housingGuestsSelectElement.value;
   if (value === 'any') {
-    return [...markerList];
+    return true;
   }
-  return markerList.filter((marker) => {
-    const element = getPopupContentElement(marker, '.popup__text--capacity');
-    const [, second] = element.firstChild.textContent.split('для');
-    const [numGests] = second.trim().split(' ');
-    return Number(numGests) === Number(value);
-  });
+  const element = getPopupContentElement(marker, '.popup__text--capacity');
+  const [, second] = element.firstChild.textContent.split('для');
+  const [numGests] = second.trim().split(' ');
+  return Number(numGests) === Number(value);
 };
 
-const filterHousingFeatures = ((markerList) => {
+const filterHousingFeatures = (marker) => {
   const filteredFeatures = Array.from(mapFeaturesCheckboxList)
     .filter((element) => element.checked)
     .map((element) => element.value)
     .sort()
     .toString();
 
-  const noticeFeatures = markerList.filter((marker) => {
-    const noticeFeatureList = Array.from(getPopupContentElement(marker, '.popup__features')
-      .querySelectorAll('.popup__feature'))
-      .map((element) => {
-        const features = [];
-        element.classList.forEach((className) => {
-          if (className.startsWith('popup__feature--')) {
-            features.push(className.replace('popup__feature--', ''));
-          }
-        });
-        return features.toString();
-      })
-      .sort()
-      .toString();
+  const noticeFeatureList = Array.from(getPopupContentElement(marker, '.popup__features')
+    .querySelectorAll('.popup__feature'))
+    .map((element) => {
+      const features = [];
+      element.classList.forEach((className) => {
+        if (className.startsWith('popup__feature--')) {
+          features.push(className.replace('popup__feature--', ''));
+        }
+      });
+      return features.toString();
+    })
+    .sort()
+    .toString();
 
-    return filteredFeatures === noticeFeatureList;
-  });
-
-  return filteredFeatures.length === 0 ? markerList : noticeFeatures;
-});
+  return filteredFeatures.length === 0 || filteredFeatures === noticeFeatureList;
+};
 
 const onFilter = () => {
-  const filterPromise = new Promise((resolve) => resolve());
-  filterPromise
-    .then(getMarkerDataList)
-    .then(filterHousingType)
-    .then(filterHousingPrice)
-    .then(filterHousingRooms)
-    .then(filterHousingGuests)
-    .then(filterHousingFeatures)
-    .then(displaySelectedMarkerList);
+  const markerList = getMarkerDataList();
+  const filteredMarkerList = markerList.filter((marker) =>
+    filterHousingType(marker)
+    && filterHousingPrice(marker)
+    && filterHousingRooms(marker)
+    && filterHousingGuests(marker)
+    && filterHousingFeatures(marker),
+  );
+
+  displaySelectedMarkerList(filteredMarkerList);
 };
 
 const onCallbackReturn = (cb) => () => cb();
